d0656feb368fb8149f703ac445806846ade715e1
rodrwankenobi <rdg8268@gmail.com>
2019-08-09 06:33:46 -0300

    Primeiro commit da aplicacao

---
+++ backend/config/server.js
@@ -0,0 +1,16 @@
+const port = 3004
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const queryParser = require('express-query-int')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+
+server.listen(port, function() {
+  console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
